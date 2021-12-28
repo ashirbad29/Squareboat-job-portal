c23445481e5771d4c5b7bacfc90c309470eaa722
ashirbad <ashirbadbehera29@gmail.com>
2021-12-28 13:06:36 +0530

    setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: ['plugin:react/recommended', 'standard', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/accessible-emoji': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-	return (
-		<div className='w-full text-center'>
-			<h1 className='mt-6 font-medium text-green-500'>Hello world</h1>
-		</div>
-	);
+  return (
+    <div className="w-full text-center">
+      <h1 className="mt-6 font-medium text-green-500">Hello world</h1>
+    </div>
+  );
 }
 
 export default App;
